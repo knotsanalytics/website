daf13ae84f2d54412ff774c9e022c71437a22659
Tommaso Laterza <tommasoltrz@gmail.com>
2021-09-29 16:30:51 +0200

    adding cms

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  generateBuildId: () => "build",
+  axios: {
+    baseURL: "/",
+  },
+  pageExtensions: ["mdx", "tsx"],
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+  },
+};
