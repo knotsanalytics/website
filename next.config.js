5e758931671696e7df713b59cb07fd05f2ada450
Tommaso Laterza <tommasoltrz@gmail.com>
2021-09-29 15:47:14 +0200

    nuked the site

--- next.config.js
+++ next.config.js
@@ -1,30 +1,4 @@
-const withMdxEnhanced = require("next-mdx-enhanced");
-const rehypePrism = require("@mapbox/rehype-prism");
-
+/** @type {import('next').NextConfig} */
 module.exports = {
-  target: "serverless",
-  i18n: {
-    locales: ["en", "fr"],
-    defaultLocale: "en",
-  },
-  axios: {
-    baseURL: "/",
-  },
-  pageExtensions: ["mdx", "tsx"],
-  webpack: (config, { buildId, dev, defaultLoaders, webpack }) => {
-    config.module.rules.push(
-      ...[
-        {
-          test: /\.yml$/,
-          type: "json",
-          use: "yaml-loader",
-        },
-        {
-          test: /\.svg$/,
-          use: "@svgr/webpack",
-        },
-      ]
-    );
-    return config;
-  },
-};
+  reactStrictMode: true,
+}
