21cb2ae43bfd8e78633e5077774ffc28b1f4f6a1
Tommaso Laterza <tommasoltrz@gmail.com>
2021-10-06 17:45:35 +0200

    added favicon and md title

--- components/meta/BasicMeta.tsx
+++ components/meta/BasicMeta.tsx
@@ -35,12 +35,12 @@ export default function BasicMeta({
       {author ? <meta name="author" content={author} /> : null}
 
       <link rel="canonical" href={config.base_url + url} />
-
+      <link rel="shortcut icon" href="/favicon.svg" />
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="manifest" href="/site.webmanifest" />
-      <link rel="apple-touch-icon" href="/icon.png" />
-      <meta name="theme-color" content="#fff" />
+      {/* <link rel="apple-touch-icon" href="/icon.png" /> */}
+      <meta name="theme-color" content="#fefefe" />
     </Head>
   );
 }
