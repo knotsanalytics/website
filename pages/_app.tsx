bf086a07947fad42c9d655f151b7b8bf4fe9a6b0
Tommaso Laterza <tommasoltrz@gmail.com>
2021-10-18 15:52:30 +0200

    added ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import "../styles/global.scss";
 import type { AppProps } from "next/app";
+import ReactGA from "react-ga";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const TRACKING_ID = "G-DZRSW2E4PP";
+  ReactGA.initialize(TRACKING_ID);
   return <Component {...pageProps} />;
 }
 export default MyApp;
