10f97137259351b939ef53bd318120716f628541
Tommaso Laterza <tommasoltrz@gmail.com>
2021-10-18 13:14:57 +0200

    fix style copyrights

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -83,146 +83,143 @@ const Contact: React.FC<ContactProps> = ({
   });
 
   return (
-    <div className={styles.contactContainer} id={"contact"}>
-      <div className={cn(`grid`, styles.grid)}>
-        <div
-          className={`col-12 col-start-md-4 col-end-md-9 col-start-lg-4 col-end-lg-9 `}
-        >
-          <h2>
-            <ReactMarkdown
-              className={cn(`fadeInUp`, styles.headline)}
-              components={{ p: "span" }}
-            >
-              {title}
-            </ReactMarkdown>
-          </h2>
-
-          <ReactMarkdown className={cn(`fadeInUp`, styles.subtitle)}>
-            {subtitle}
-          </ReactMarkdown>
-        </div>
-
-        <div
-          className={`col-12 col-start-md-4 col-end-md-9 col-start-lg-4 col-end-lg-9`}
-        >
-          <form
-            id="contact-form"
-            className={styles.form}
-            onSubmit={formik.handleSubmit}
-            encType="text/plain"
-            action="#"
-            method="POST"
+    <div className={styles.contactSpacer} id={"contact"}>
+      <div className={styles.contactContainer}>
+        <div className={cn(`grid`, styles.grid)}>
+          <div
+            className={`col-12 col-start-md-4 col-end-md-9 col-start-lg-4 col-end-lg-9 `}
           >
-            <div className={styles.inputWrap}>
-              <p className={cn(styles.error, "small")}>
-                {getVisibleError(formik, "name")}
-              </p>
+            <h2>
+              <ReactMarkdown
+                className={cn(`fadeInUp`, styles.headline)}
+                components={{ p: "span" }}
+              >
+                {title}
+              </ReactMarkdown>
+            </h2>
 
-              <input
-                type="text"
-                className={`${styles.formControl} fadeInUp`}
-                id={"name"}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.name}
-                name={"name"}
-                disabled={loading}
-                placeholder={contactForm.nameLabel}
-              />
-            </div>
+            <ReactMarkdown className={cn(`fadeInUp`, styles.subtitle)}>
+              {subtitle}
+            </ReactMarkdown>
+          </div>
 
-            <div className={styles.inputWrap}>
-              <p className={cn(styles.error, "small")}>
-                {getVisibleError(formik, "email")}
-              </p>
-              <input
-                type="email"
-                className={`${styles.formControl} fadeInUp`}
-                id={"email"}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
-                name={"email"}
-                disabled={loading}
-                placeholder={contactForm.emailLabel}
-              />
-            </div>
-
-            <div className={styles.areaWrap}>
-              <p className={cn(styles.error, "small")}>
-                {getVisibleError(formik, "message")}
-              </p>
-              <textarea
-                className={`${styles.formControl} fadeInUp`}
-                id={"message"}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.message}
-                name={"message"}
-                disabled={loading}
-                placeholder={contactForm.messageLabel}
-              />
-            </div>
-
-            <Button
-              label={contactForm.ctaLabel}
-              type="submit"
-              className={cn(styles.cta, "fadeInUp")}
-              disabled={loading || !formik.isValid || !formik.dirty}
-              secondary
-            />
-          </form>
-        </div>
-      </div>
-      <footer className={styles.footer}>
-        <div className="grid">
-          <div className={styles.itemsContainer}>
-            <a
-              className={styles.footerItem}
-              href={linkedin}
-              rel="noopener"
-              target="_blank"
+          <div
+            className={`col-12 col-start-md-4 col-end-md-9 col-start-lg-4 col-end-lg-9`}
+          >
+            <form
+              id="contact-form"
+              className={styles.form}
+              onSubmit={formik.handleSubmit}
+              encType="text/plain"
+              action="#"
+              method="POST"
             >
-              <Icon icon="linkedin" className={styles.footerIcon} />
-              <p className="small">{"Linkedin"}</p>
-            </a>
-            <div className={styles.footerItem}>
-              <Icon icon="phone" className={styles.footerIcon} />
-              <p className="small">{phone}</p>
-            </div>
-            <div className={styles.footerItem}>
-              <Icon icon="location" className={styles.footerIcon} />
-              <p className="small">{address}</p>
-            </div>
+              <div className={styles.inputWrap}>
+                <p className={cn(styles.error, "small")}>
+                  {getVisibleError(formik, "name")}
+                </p>
+
+                <input
+                  type="text"
+                  className={`${styles.formControl} fadeInUp`}
+                  id={"name"}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.name}
+                  name={"name"}
+                  disabled={loading}
+                  placeholder={contactForm.nameLabel}
+                />
+              </div>
+
+              <div className={styles.inputWrap}>
+                <p className={cn(styles.error, "small")}>
+                  {getVisibleError(formik, "email")}
+                </p>
+                <input
+                  type="email"
+                  className={`${styles.formControl} fadeInUp`}
+                  id={"email"}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  name={"email"}
+                  disabled={loading}
+                  placeholder={contactForm.emailLabel}
+                />
+              </div>
+
+              <div className={styles.areaWrap}>
+                <p className={cn(styles.error, "small")}>
+                  {getVisibleError(formik, "message")}
+                </p>
+                <textarea
+                  className={`${styles.formControl} fadeInUp`}
+                  id={"message"}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.message}
+                  name={"message"}
+                  disabled={loading}
+                  placeholder={contactForm.messageLabel}
+                />
+              </div>
+
+              <Button
+                label={contactForm.ctaLabel}
+                type="submit"
+                className={cn(styles.cta, "fadeInUp")}
+                disabled={loading || !formik.isValid || !formik.dirty}
+                secondary
+              />
+            </form>
           </div>
         </div>
-      </footer>
-
-      <div className={styles.copyrights}>
-        <div className={cn("grid", styles.grid)}>
-          <div className={styles.flex}>
-            <p className={styles.rights}>
-              © Copyright 2021 Knots. All rights reserved.
-            </p>
-            <p className={styles.rights}>
-              Designed and developed by{" "}
+        <footer className={styles.footer}>
+          <div className="grid">
+            <div className={"col-12 col-md-6"}>
               <a
-                href="https://tommasolaterza.io"
+                className={styles.footerItem}
+                href={linkedin}
                 rel="noopener"
                 target="_blank"
               >
-                Tommaso Laterza
+                <Icon icon="linkedin" className={styles.footerIcon} />
+                <p className="small">{"Linkedin"}</p>
               </a>
-            </p>
+              <div className={styles.footerItem}>
+                <Icon icon="phone" className={styles.footerIcon} />
+                <p className="small">{phone}</p>
+              </div>
+              <div className={styles.footerItem}>
+                <Icon icon="location" className={styles.footerIcon} />
+                <p className="small">{address}</p>
+              </div>
+            </div>
+            <div className={"col-12 col-md-6"}>
+              <p className={styles.rights}>
+                © Copyright 2021 Knots. All rights reserved.
+              </p>
+              <p className={styles.rights}>
+                Designed and built by{" "}
+                <a
+                  href="https://tommasolaterza.io"
+                  rel="noopener"
+                  target="_blank"
+                >
+                  Tommaso Laterza
+                </a>
+              </p>
+            </div>
           </div>
-        </div>
-      </div>
+        </footer>
 
-      <Dialog active={messageSent} onClose={() => setMessageSent(false)}>
-        <div className={styles.dialogContainer}>
-          <ReactMarkdown>{contactForm.confirmationMessage}</ReactMarkdown>
-        </div>
-      </Dialog>
+        <Dialog active={messageSent} onClose={() => setMessageSent(false)}>
+          <div className={styles.dialogContainer}>
+            <ReactMarkdown>{contactForm.confirmationMessage}</ReactMarkdown>
+          </div>
+        </Dialog>
+      </div>
     </div>
   );
 };
